3f16ad98b02e5dc9c70b5335472f2480ba41ede0
Muhammed Basil <basil@montycloud.com>
2021-07-26 03:10:07 +0530

    route fix

---
+++ docs/build/reroute.js
@@ -0,0 +1,9 @@
+function getQueryStringValue(key) {
+	return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+}
+
+const redirect = getQueryStringValue("redirect");
+console.log("redirect", redirect)
+if (document.location.pathname === '/' && redirect) {
+	document.location.replace(`${document.location.origin}/#/${redirect}`);
+}

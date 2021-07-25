ea394b20ddcadb34c75d80fafbbe1cdbfda371d2
Muhammed Basil <basil@montycloud.com>
2021-07-26 02:53:53 +0530

    reroute

--- build/reroute.js
+++ build/reroute.js
@@ -1,5 +1,3 @@
-	x// import queryString from 'query-string'
-
 function getQueryStringValue(key) {
 	return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 }

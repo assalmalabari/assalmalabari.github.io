ee4591feb0aeefeb378ee2eb2fe4932edda37fe6
Muhammed Basil <basil@montycloud.com>
2021-07-26 02:29:44 +0530

    Path reroute

---
+++ public/reroute.js
@@ -0,0 +1,11 @@
+// import queryString from 'query-string'
+
+function getQueryStringValue(key) {
+	return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+}
+
+const redirect = getQueryStringValue("redirect");
+console.log("redirect", redirect)
+if (document.location.pathname === '/' && redirect) {
+	document.location.assign(`${document.location.origin}/${redirect}`);
+}
